4193916561eaf53e9eb7bf4065e8e33264ef00ff
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-03-05 01:10:11 -0500

    leer metadatos del archivo mp3

--- script.js
+++ script.js
@@ -13,4 +13,47 @@ document.getElementById('play-btn').addEventListener('click', function() {
     }
 });
 
- 
+
+//LEER INFO DEL ARCHIVO MP3
+document.getElementById('file-input').addEventListener('change', function(event) {
+    var input = event.target;
+    if (input.files && input.files.length > 0) {
+        var file = input.files[0];
+        if (file.name.toLowerCase().endsWith('.mp3')) {
+            console.log('Archivo MP3 seleccionado:', file.name);
+            // Leer los metadatos del archivo MP3
+            jsmediatags.read(file, {
+                onSuccess: function(tag) {
+                    var title = tag.tags.title;
+                    var artist = tag.tags.artist;
+                    var album = tag.tags.album;
+                    var picture = tag.tags.picture;
+
+                    document.getElementById('file-name').textContent = 'Title: ' + title;
+                    document.getElementById('file-artist').textContent = 'Artist: ' + artist;
+                    document.getElementById('file-duration').textContent = 'Album: ' + album;
+
+                    // Verificar si la imagen existe antes de mostrarla
+                    if (picture) {
+                        var base64String = '';
+                        for (var i = 0; i < picture.data.length; i++) {
+                            base64String += String.fromCharCode(picture.data[i]);
+                        }
+                        var imageUrl = 'data:' + picture.format + ';base64,' + window.btoa(base64String);
+                        var imgElement = document.getElementById('file-image');
+                        imgElement.src = imageUrl;
+                        imgElement.style.display = 'block';
+                    } else {
+                        // Si no hay imagen, ocultar el contenedor de la imagen
+                        document.getElementById('file-image').style.display = 'none';
+                    }
+                },
+                onError: function(error) {
+                    console.log('Error leyendo los metadatos:', error);
+                }
+            });
+        } else {
+            alert('Por favor, selecciona un archivo MP3.');
+        }
+    }
+});
